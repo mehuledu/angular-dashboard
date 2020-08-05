520ea910296ea0ecc4abd342caa2ab1062c2b71a
mehuledu <mehuledu@yahoo.com>
2020-08-05 17:44:11 -0400

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { BookmarkComponent } from './bookmark/bookmark.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule ],
   declarations: [ AppComponent, BookmarkComponent ],
   bootstrap:    [ AppComponent ]
 })
