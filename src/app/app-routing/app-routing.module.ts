a72b68f7b9eb8e92966fc0bcc1ecf075a181eafd
mehuledu <mehuledu@yahoo.com>
2020-11-08 23:43:40 -0500

    4 files modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router"; // CLI imports router
+import { SamsungNavbarComponent } from "../samsung-navbar/samsung-navbar.component";
 
 import { BookmarkComponent } from "./../bookmark/bookmark.component";
-import { PageNotFoundComponent } from './../page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from "./../page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: "first-component", component: BookmarkComponent },
   { path: "bookmark", component: BookmarkComponent },
+  { path: "samsung", component: SamsungNavbarComponent },
   { path: "", redirectTo: "/bookmark", pathMatch: "full" },
   { path: "**", component: BookmarkComponent }
 ];
