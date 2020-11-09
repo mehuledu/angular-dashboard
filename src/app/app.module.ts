854081e2f9633236cb1dc234b88df1178127ecde
mehuledu <mehuledu@yahoo.com>
2020-11-08 23:27:12 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { Router } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-
-import { BookmarkComponent } from './bookmark/bookmark.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SamsungNavbarComponent } from './samsung-navbar/samsung-navbar.component';
-import { JsonDataService } from './services/json-data.service';
+import { BookmarkComponent } from "./bookmark/bookmark.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { SamsungNavbarComponent } from "./samsung-navbar/samsung-navbar.component";
+import { JsonDataService } from "./services/json-data.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule, AppRoutingModule,HttpClientModule],
-  declarations: [ AppComponent, BookmarkComponent, NavbarComponent, PageNotFoundComponent, SamsungNavbarComponent ],
-  bootstrap:    [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  declarations: [
+    AppComponent,
+    BookmarkComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    SamsungNavbarComponent
+  ],
+  bootstrap: [AppComponent],
   providers: [JsonDataService]
 })
-export class AppModule { }
+export class AppModule {}
